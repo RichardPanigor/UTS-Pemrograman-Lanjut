a64d09ac528f9a0f2fce53c7f82e46b3aad0e611
RichardPanigor <80900416+RichardPanigor@users.noreply.github.com>
2021-04-22 09:56:06 +0700

    Add files via upload

---
+++ Hewan/Hewan.java
@@ -0,0 +1,21 @@
+// Nama  : Richard Panigor Sitompul
+// NIM   : 11200930000025
+// Kelas : 2A-Sistem Informasi
+// Jawaban soal no.4
+
+package Hewan;
+
+public class Hewan {
+    void namaHewan(String nama){
+        System.out.println("Nama Hewan : " + nama);
+    }
+    void jenisHewan(String jenis){
+        System.out.println("Jenis Hewan : " + jenis);
+    }
+    void jumlahKaki(int kaki){
+        System.out.println("Jumlah Kaki : " + kaki);
+    }
+    void warnaHewan(String warna){
+        System.out.println("Warna Hewan : " + warna);
+    }
+}
---
+++ Hewan/Sapi.java
@@ -0,0 +1,17 @@
+// Nama  : Richard Panigor Sitompul
+// NIM   : 11200930000025
+// Kelas : 2A-Sistem Informasi
+// Jawaban soal no.4
+
+package Hewan;
+
+public class Sapi {
+    public static void main(String[] args){
+        Hewan sapi = new Hewan();
+        sapi.namaHewan("Gajah");
+        sapi.jenisHewan("Herbivora");
+        sapi.jumlahKaki(4);
+        sapi.warnaHewan("Putih");
+    }
+    
+}
